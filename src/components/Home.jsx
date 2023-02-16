a88cb54a4296d6d6ecd9db02e1d61819e9fbbe99
matthew <larochelle.matthew@gmail.com>
2023-02-16 15:14:18 -0800

    map product cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,36 +2,20 @@ import React from "react";
 import Product from "./Product.jsx";
 import products from "../products";
 
+function createCard(product) {
+    return <Product 
+        title={product.title}
+        imgURL={product.imgURL}
+        alt={product.alt}
+        description={product.description}
+    />
+}
+
 function Home() {
     return (
         <div className="row">
             <div className="column">
-            <Product
-                title={products[0].title}
-                imgURL={products[0].imgURL}
-                description={products[0].description}
-            />
-            </div>
-            <div className="column">
-            <Product 
-                title={products[1].title}
-                imgURL={products[1].imgURL}
-                description={products[1].description}
-            />
-            </div>
-            <div className="column">
-            <Product 
-                title={products[2].title}
-                imgURL={products[2].imgURL}
-                description={products[2].description}
-            />
-            </div>
-            <div className="column">
-            <Product 
-                title={products[3].title}
-                imgURL={products[3].imgURL}
-                description={products[3].description}
-            />
+            {products.map(createCard)}
             </div>
         </div>
     );
